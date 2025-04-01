c8574a02912644e1f1e057c9e3a13459f2681ca9
Wooyoung82760 <wooyoung82760@gmail.com>
2025-04-01 10:44:54 +0900

    부트스트랩 적용

--- blog/views.py
+++ blog/views.py
@@ -6,30 +6,30 @@ from .models import Post
 class PostList(ListView):
     model = Post
     ordering = '-pk'
-
+    template_name='bolg/post_list.html'
 
 class PostDetail(DetailView):
     model = Post
 
-def index(request):
-    posts = Post.objects.all().order_by('-pk')
-
-    return render(
-        request,
-        'blog/index.html',
-        {
-            'posts': posts,
-        }
-    )
-
-
-def single_post_page(request, pk):
-    post = Post.objects.get(pk=pk)
-
-    return render(
-        request,
-        'blog/single_post_page.html',
-        {
-            'post': post,
-        }
-    )
+# def index(request):
+#     posts = Post.objects.all().order_by('-pk')
+#
+#     return render(
+#         request,
+#         'blog/post_list.html',
+#         {
+#             'posts': posts,
+#         }
+#     )
+#
+#
+# def single_post_page(request, pk):
+#     post = Post.objects.get(pk=pk)
+#
+#     return render(
+#         request,
+#         'blog/single_post_page.html',
+#         {
+#             'post': post,
+#         }
+#     )
