5661506dbb384cb0a43bd656eaecade132ebee52
Wooyoung82760 <wooyoung82760@gmail.com>
2025-04-02 15:45:14 +0900

    post_list, post_detail <<add tag

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,15 @@
+from bs4 import Tag
 from django.db import models
 from django.contrib.auth.models import User
 import os
 
-
+class Tag (models.Model):
+    name=models.CharField(max_length=50)
+    slug=models.SlugField(max_length=200,unique=True,allow_unicode=True)
+    def __str__(self):
+        return self.name
+    def get_absolute_url(self):
+        return f'/blog/tag/{self.slug}/'
 
 class Category(models.Model):
     name=models.CharField(max_length=50,unique=True)
@@ -32,6 +39,8 @@ class Post(models.Model):
 
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
 
+    tags = models.ManyToManyField(Tag, blank=True)
+
     def __str__(self):
         return f'[{self.pk}]{self.title} :: {self.author}'
 
@@ -43,3 +52,5 @@ class Post(models.Model):
 
     def get_file_ext(self):
         return self.get_file_name().split('.')[-1]
+
+
