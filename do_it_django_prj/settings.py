b72809091750ff84989b9ff50bd3384f870ad2bb
Wooyoung82760 <wooyoung82760@gmail.com>
2025-04-03 15:50:52 +0900

    crispy-forms apply

--- do_it_django_prj/settings.py
+++ do_it_django_prj/settings.py
@@ -37,8 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    "crispy_forms",
+    "crispy_bootstrap5",
+    
     'blog','single_pages',
-    #'django_extensions',
 ]
 
 MIDDLEWARE = [
@@ -121,6 +124,9 @@ STATIC_URL = 'static/'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
